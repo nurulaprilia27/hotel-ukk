5196657b448da35d1c083a348b0aa442d0d632ae
nurulaprilia27 <nurulaprilia610@gmail.com>
2022-03-11 10:35:53 +0700

    tambahassetadmin

---
+++ database/migrations/2022_03_10_073526_create_kamars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKamarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamars', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kode_kamar');
+            $table->string('nama_kamar');
+            $table->integer('harga_malam');
+            $table->integer('jumlah_kamar');
+            $table->foreignId('tipe_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamars');
+    }
+}
