0569e4604c5748729e8172c33d504aba12594ae7
muhammad arya hidayat <aryapakehidayat@gmail.com>
2022-03-17 14:04:32 +0700

    update view index fasilitas

--- resources/views/admin/fasilitas_kamar/index.blade.php
+++ resources/views/admin/fasilitas_kamar/index.blade.php
@@ -1,58 +1,54 @@
 @extends('layouts.app')
-@section('content') 
+@section('content')
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nama Fasilitas</th>
-                                <th scope="col">action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td style="width: 20%" class="text-center">
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <a href="" class="btn btn-primary btn-sm">
-                                            <i class="far fa-edit"></i>
-                                        </a>
-                                        <button type="submit" class="btn btn-danger btn-sm"
-                                            onclick="document.getElementById('form_delete').submit();">
-                                            <i class="far fa-trash-alt"></i>
-                                        </button>
-                                    </div>
-                                    <form action="" id="form_delete" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                    </form>
-                            </tr>
-                        </tbody>
-                    </table>
+            <div class="card mb-4">
+                <div class="card-header pb-0">
+                    <h6>Fasilitas Kamar</h6>
                 </div>
-            </div>
-        </div>
-    </div>
-    {{-- <footer class="footer pt-3  ">
-        <div class="container-fluid">
-            <div class="row align-items-center justify-content-lg-between">
-                <div class="col-lg-6 mb-lg-0 mb-4">
-                    <div class="copyright text-center text-sm text-muted text-lg-start">
-                        Â© <script>
-                            document.write(new Date().getFullYear())
-                        </script>,
-                        made with <i class="fa fa-heart"></i> by
-                        <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative
-                            Tim</a>
-                        for a better web.
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0 text-center">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                                        width="5%">
+                                        No</th>
+                                    <th
+                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Nama Fasilitas</th>
+                                    <th class="text-secondary opacity-7"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">Manager</p>
+                                    </td>
+                                    <td class="align-middle" width='20%'>
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <a href="" class="btn btn-primary btn-sm">
+                                                <i class="far fa-edit"></i>
+                                            </a>
+                                            <button type="submit" class="btn btn-danger btn-sm"
+                                                onclick="document.getElementById(' form_delete').submit();">
+                                                <i class="far fa-trash-alt"></i>
+                                            </button>
+                                        </div>
+                                        <form action="" id="form_delete" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    </footer> --}}
+    </div>
+</div>
 @endsection
--- resources/views/layouts/template/navbar.blade.php
+++ resources/views/layouts/template/navbar.blade.php
@@ -1,44 +1,51 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
-            navbar-scroll="true">
-            <div class="container-fluid py-1 px-3">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a>
-                        </li>
-                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
-                    </ol>
-                    <h6 class="font-weight-bolder mb-0">Dashboard</h6>
-                </nav>
-                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                    </div>
-                    <ul class="navbar-nav  justify-content-end">
-                        <li class="nav-item d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-body font-weight-bold px-3">
-                                <i class="fa fa-user me-sm-1"></i>
-                                <span class="d-sm-inline d-none">{{ auth()->user()->name }}</span>
-                            </a>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();"
-                                class="nav-link text-body font-weight-bold px-0">
-                                out >
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                                <div class="sidenav-toggler-inner">
-                                    <i class="sidenav-toggler-line"></i>
-                                    <i class="sidenav-toggler-line"></i>
-                                    <i class="sidenav-toggler-line"></i>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    navbar-scroll="true">
+    <div class="container-fluid py-1 px-3">
+        <nav class="page-header">
+            <ol class="breadcrumb">
+                <!-- breadcrumb -->
+                <li class="breadcrumb-item"><a href="">Home</a></li>
+                <?php
+                            $segments = '';
+                            $total_segments = count(Request::segments());
+                        ?>
+                @foreach(Request::segments() as $key => $segment)
+                <?php $segments .= '/'.$segment; ?>
+                <li class="breadcrumb-item {{ $key+1 == $total_segments ? 'active' : ''}}" aria-current="page">{{
+                    ucwords(str_replace('_', ' ',$segment)) }}</li>
+                @endforeach
+            </ol>
         </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            </div>
+            <ul class="navbar-nav  justify-content-end">
+                <li class="nav-item d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-body font-weight-bold px-3">
+                        <i class="fa fa-user me-sm-1"></i>
+                        <span class="d-sm-inline d-none">{{ auth()->user()->name }}</span>
+                    </a>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                        class="nav-link text-body font-weight-bold px-0">
+                        out >
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
